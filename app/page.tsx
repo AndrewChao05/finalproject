b7b1a09c6b8e1cf29ccd44f5797bec6f7f67418c
AndrewChao05 <andrew20051019@gmail.com>
2025-05-17 21:51:23 +0800

    add StartScreen, save gameover logic for later

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,33 @@
 
 import './globals.css';
 
-import React from 'react';
+import React,{ useState } from 'react';
 
 import Game from '../components/Game';
+import StartScreen from '../components/StartScreen';
+//import GameOverScreen from '../components/GameOverScreen';
 
 export default function Home() {
+  const [gameState, setGameState] = useState<'start' | 'playing' | 'gameover'>('start');
+  const [score, setScore] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  
+
+  const handleStart = () => {
+    setScore(0);
+    setIsRunning(true);
+    setGameState('playing');
+  };
+
+  const handleGameOver = () => {
+    setGameState('gameover');
+  };
+
   return (
-    <main className="flex justify-center items-center min-h-screen bg-gray-100">
-      <Game />
+    <main className="flex justify-center items-center w-full h-screen bg-gray-100">
+      {gameState === 'start' && <StartScreen onStart={handleStart} />}
+      {gameState === 'playing' && <Game onGameOver={handleGameOver} score={score} setScore={setScore} isRunning={isRunning}/>}
+      {/* {gameState === 'gameover' && <GameOverScreen score={score} onRestart={handleStart} />} */}
     </main>
   );
 }
